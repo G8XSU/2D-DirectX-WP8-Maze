d3c65bd099f098e0d4432ca66bf57a98525c0476
Gursharan Singh <g8x000@gmail.com>
2014-09-08 22:26:54 +0530

    Ball class done

---
+++ dxwpMaze/dxwpMaze/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using dxwpMaze.Resources;
+
+namespace dxwpMaze
+    {
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+        {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+        }
+    }
