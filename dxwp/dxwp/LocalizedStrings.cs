f3e52e97c13e1960f31ef80d059ecf235572af0f
Gursharan Singh <g8x000@gmail.com>
2014-09-08 19:32:19 +0530

    ball class done

---
+++ dxwp/dxwp/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using dxwp.Resources;
+
+namespace dxwp
+    {
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+        {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+        }
+    }
